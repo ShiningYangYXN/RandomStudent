0b8e0dbd15d08873de77e13275d15ffc99d9fef4
Shining Yang <ShiningYangYXN@163.com>
2024-05-18 12:13:31 +0000

    2024年5月18日 20:13

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -50,6 +50,7 @@ function Stop() {
         :interval="100"
         :autoplay="running"
         indicator-position="none"
+        pause-on-hover="false"
         :motion-blur="config.data.enableBlur"
       >
         <el-carousel-item v-for="item in nameList" :key="nameList.indexOf(item)">
